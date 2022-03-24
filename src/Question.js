4d176c70050f84abcd37b6d16e1c124e02591f77
mafhhend <you@example.com>
2022-03-25 02:19:26 +0430

    use react-icons

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { FaTimes,FaPlus } from "react-icons/fa";
 const Question = ({ title, info }) => {
   const [isVisible, setIsVisible] = useState(false);
   const infoClassName=` white bg-rose-100 p-4 ${isVisible ? '' : 'none'}`;
@@ -7,8 +7,9 @@ const Question = ({ title, info }) => {
   return (
     <li className="mt-2" onClick={ToggleVisible}>
       <div className="flex flex-between">
-        <h5>{title}</h5>
-        <button className="btn" onClick={ToggleVisible}>{isVisible ? "-" : "+"}</button>
+        <h5>{title} 
+        </h5>
+        <button className="btn" onClick={ToggleVisible}>{isVisible ? <FaTimes/> : <FaPlus/>}</button>
       </div>
       <p className={infoClassName}>{info}</p>
     </li>
